34b9275e3636b7928fccff4dac366f53cc9fc3b1
Márk Oroszi <mark@outlast.io>
2017-12-05 20:23:14 +0100

    started client app

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    app: {
+        port: 3000,
+        srcPath: 'app',
+        distPath: 'dist',
+        publicPath: 'public'
+    },
+    api: {
+        port: 3001
+    }
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,35 @@
-module.exports = {
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const config = require('./config');
 
-}
+module.exports = {
+    entry: ['babel-polyfill', path.join(__dirname, config.app.srcPath, 'app.jsx')],
+    output: {
+        filename: 'bundle-[hash].js',
+        path: path.join(__dirname, config.app.distPath)
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?/,
+                use: 'babel-loader'
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: path.join(__dirname, config.app.distPath, 'index.html'),
+            template: path.join(__dirname, config.app.publicPath, 'index.html')
+        })
+    ],
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    devtool: 'cheap-eval-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, config.app.distPath),
+        historyApiFallback: true,
+        inline: true,
+        port: config.app.port
+    }
+};
